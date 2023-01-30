b0030a87faf00f2b1a84f6145bfeb89b3d2a8bcf
blindcoder123 <blind1371@gmail.com>
2023-01-30 15:59:57 -0500

    Added some more display functionality

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function addition(a, b) {
-    console.log(a+b);
     return a + b;
 }
 
@@ -40,101 +39,164 @@ clear.addEventListener('click', () => {
     leftVal = 0;
     rightVal = 0;
     opCode = '';
+    display.removeChild(display.firstChild);
 })
 
 const one = document.querySelector('#One');
+
 one.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }        
     if(opCode == '') {
         leftVal = 1;
     } else {
         rightVal = 1;
     }
+    display.append('1');
 });
 
 const two = document.querySelector('#Two');
 two.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode == '') {
         leftVal = 2;
     } else {
         rightVal = 2;
     }
+    display.append('2');
 });
 
 const three = document.querySelector('#Three');
 three.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode = '') {
         leftVal = 3;
     } else {
         rightVal = 3;
     }
+    display.append('3');
 });
 
 const four = document.querySelector('#Four');
 four.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode = '') {
         leftVal = 4;
     } else {
         rightVal = 4;
     }
+    display.append('4');
 });
 
 const five = document.querySelector('#Five');
 five.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode = '') {
         leftVal = 5;
     } else {
         rightVal = 5;
     }
+    display.append('5');
 });
 
 const six = document.querySelector('#Six');
 six.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode = '') {
         leftVal = 6;
     } else {
         rightVal = 6;
     }
+    display.append('6');
 });
 
 const seven = document.querySelector('#Seven');
 seven.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode = '') {
         leftVal = 7;
     } else {
         rightVal = 7;
     }
+    display.append('7');
 });
 
 const eight = document.querySelector('#Eight');
 eight.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode = '') {
         leftVal = 8;
     } else {
         rightVal = 8;
     }
+    display.append('8');
 });
 
 const nine = document.querySelector('#Nine');
 nine.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode = '') {
         leftVal = 9;
     } else {
         rightVal = 9;
     }
+    display.append('19');
 });
 
 const zero = document.querySelector('#Zero');
 zero.addEventListener('click', () => {
+    if(opCode == '+' || opCode == '-' || opCode == '*' || opCode == '/') {
+        while(display.firstChild) {
+            display.removeChild(display.firstChild)
+        }       
+    }   
     if(opCode = '') {
         leftVal = 0;
     } else {
         rightVal = 0;
     }
+    display.append('0');
 });
 
 const add = document.querySelector('#Add');
 add.addEventListener('click', () => {
     opCode = '+';
+    display.append('+');
 });
 
 const subtract = document.querySelector('#Subtract');
@@ -154,6 +216,9 @@ divide.addEventListener('click', () => {
 
 const equals = document.querySelector('#Equals');
 equals.addEventListener('click', () => {
+    while(display.firstChild) {
+        display.removeChild(display.firstChild)
+    }   
     content.textContent = operate(opCode, leftVal, rightVal);
     display.append(content);
 })
